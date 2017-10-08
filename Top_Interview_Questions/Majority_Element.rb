8d504dabcee2116a0d8b236b5e8c75469879b703
Allan James Lapid <ajLapid718@gmail.com>
2017-10-08 01:01:47 -0400

    Find the majority element

---
+++ Top_Interview_Questions/Majority_Element.rb
@@ -0,0 +1,19 @@
+# Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+# You may assume that the array is non-empty and the majority element always exist in the array.
+
+# My solution with a runtime of 80ms
+def majority_element(nums)
+    counter_hash = Hash.new(0)
+
+    nums.each do |num|
+        counter_hash[num] += 1
+    end
+
+    counter_hash.find { |k,v| v == counter_hash.values.max }.first
+end
+
+# Top Solution with a better runtime
+def majority_element(nums)
+  return nums.first if nums.size == 1
+  return nums.sort[nums.size/2]
+end
