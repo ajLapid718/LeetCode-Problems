aa432b1e5b936b33c60435b3b2a4c731827100ae
Allan James Lapid <ajLapid718@gmail.com>
2017-10-17 03:50:41 -0400

    Reverse a singly-linked list

--- Top_Interview_Questions/Reverse_A_Linked_List.rb
+++ Top_Interview_Questions/Reverse_A_Linked_List.rb
@@ -16,8 +16,7 @@ def reverse_list(head)
 
   until current_node == nil # This line traverses the entire singly-linked list; this involves reversing the nodes through each iteration in an as-we-go manner.
       next_node = current_node.next # It is semantic; the next node is set to point to the value that the current node's #next would output.
-      current_node.next = previous_node # Set the actual next node to point toward the previous node.
-
+      current_node.next = previous_node # Set the actual next node to point toward the previous node. This is where the bulk of the reversal process happens.
       previous_node = current_node # Set the previous node to point toward the actual current node at hand...which is going to become the new previous node for the next iteration.
       current_node = next_node # Set the current node to point toward the next node; in a swap, the current_node does become the next_node. This allows us to move on. The next iteration will have this as a starting point.
   end # After all of this, the end of the linked list has been reached. Ultimately, the original tail needs to become the new head.
@@ -31,9 +30,18 @@ nil -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> nil
 [x] it is prudent to start at the head of a linked list in this case, so the very first current_node, which will change as-we-go, is the head (1)
 [x] we'll need to traverse the entire singly linked list, which means we'll need a stopping point; after (6) we have value of nil and this is the stopping point for the until loop
 [x] we need a variable (next_node) to consistently point toward the value of the following node (2); with this, we can hold a stable reference to the proper starting point for the next run-through of the loop
-[x] we need the actual following node (2) to be set with a value of the previous_node (nil); this is necessary because everything in a linked list points toward something ahead of it...so this redirects that arrow so-to-speak
+[x] we need the actual following node (2) to be set with a value of the previous_node (nil); this is necessary because everything in a linked list points toward something ahead of it AND does not point to anything prior to it (unless it is a doubly-linked list)...so this redirects that arrow so-to-speak
 [x] we need to do some cleanup and preparation for the next iteration in the spirit of foresight; the previous_node is set as the current_node (1) at hand
-[x] we need to do some more cleanup and preparation...the current_node for the next run-through (2) will be set to the next_node, which was holding/pointing to the value in the beginning and is unaffected by the changes that just took place	
+[x] we need to do some more cleanup and preparation...the current_node for the next run-through (2) will be set to the next_node, which was holding/pointing to the value in the beginning and is unaffected by the changes that just took place
 [x] the final touch is to set (the original tail) whatever was the last node (previous_node) as the new head (6)
 
-nil -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> nil
+6 -> 5 -> 4 -> 3 -> 2 -> 1 -> nil
+
+# LeetCode Explanation
+
+Assume that we have linked list 1 → 2 → 3 → Ø, we would like to change it to Ø ← 1 ← 2 ← 3.
+
+While you are traversing the list, change the current node's next pointer to point to its previous element.
+Since a node does not have reference to its previous node, you must store its previous element beforehand.
+You also need another pointer to store the next node before changing the reference.
+Do not forget to return the new head reference at the end!
