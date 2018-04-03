bfa27dc84e01603519a7ed426c79c93acaf1ebce
Allan James Lapid <ajLapid718@gmail.com>
2018-04-03 14:28:03 -0400

    Use a hash map and bucket sort to grab top k elements

---
+++ Top_Interview_Questions/topKFrequentElements.js
@@ -0,0 +1,50 @@
+/*
+
+Given a non-empty array of integers, return the k most frequent elements.
+
+For example,
+Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+*/
+
+function topKFrequent(nums, k) {
+  let result = [];
+  let bucket = [];
+  let obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    obj[num] = obj[num] + 1 || 1;
+    bucket[i+1] = null;
+  }
+
+  for (let key in obj) {
+    let val = obj[key];
+
+    if (bucket[val] === null) {
+      bucket[val] = [key];
+    }
+    else {
+      bucket[val].push(key);
+    }
+  }
+
+  let counter = 0;
+
+  for (let j = bucket.length-1; j > 0; j--) {
+    if (bucket[j] !== null) {
+      result.push(bucket[j]);
+      counter += bucket[j].length;
+    }
+
+    if (counter >= k) {
+      break;
+    }
+  }
+
+  return result.join(',').split(',').map(el => parseInt(el));
+}
