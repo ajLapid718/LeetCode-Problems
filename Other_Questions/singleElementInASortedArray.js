d75cc4b59b7e8b6e501990aabba835e52a9a30b7
Allan James Lapid <ajLapid718@gmail.com>
2018-04-03 23:33:46 -0400

    Find lone number among duplicates in a sorted array

---
+++ Other_Questions/singleElementInASortedArray.js
@@ -0,0 +1,29 @@
+/*
+
+Given a sorted array consisting of only integers where every element appears twice except for one element which appears once.
+Find this single element that appears only once.
+
+Example 1:
+
+Input: [1,1,2,3,3,4,4,8,8]
+Output: 2
+
+Example 2:
+
+Input: [3,3,7,7,10,11,11]
+Output: 10
+
+Note: Your solution should run in O(log n) time and O(1) space.
+
+*/
+
+function singleNonDuplicate(nums) {
+  for (let i = 0; i < nums.length; i+=2) {
+    let curr = nums[i];
+    let folo = nums[i+1];
+
+    if (curr !== folo) {
+      return curr;
+    }
+  }
+}
