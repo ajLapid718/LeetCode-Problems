c82d6be810329c5c12ec1c1ae5834c2dcb692279
Allan James Lapid <ajLapid718@gmail.com>
2018-08-01 17:17:45 -0400

    Begin a LL with a non-matching value and drop refs to matches

--- Other_Questions/removeLinkedListElements.js
+++ Other_Questions/removeLinkedListElements.js
@@ -10,12 +10,13 @@ Output: 1->2->3->4->5
 */
 
 // My solution;
-// If we don't have a falsy value is passed in as a LL, return that falsy value;
+// If we a falsy value is passed in as a LL, return that falsy value;
 // While we have a head node and while the data on the node matches the target value, keep sliding through the LL;
-// This allows us to clear up any matching values at the front of the list, allowing us to start with a non-matching value;
-// If the head is a falsy value at this point, then all of the values were matching, which means we slid over to the tail null;
+// This allows us to ignore any matching values at the front of the list, thereby allowing us to start with a non-matching value;
+// If the head is a falsy value at this point, then all of the values were matching, which means we slid over to the tail-end null;
 // If this is the case, then we simply have to return null as prompted;
 // Otherwise, we traverse the remainder of the LL and drop the next node if its data matches the target value;
+
 function removeElements(head, val) {
   if (!head) return head;
 
