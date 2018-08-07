28090bb994816c9b7859240329a3ddeb39be129f
Allan James Lapid <ajLapid718@gmail.com>
2018-08-07 01:20:56 -0400

    Use nested loops to ignore the respective value at a certain index

---
+++ Top_Interview_Questions/productOfArrayExceptSelf.js
@@ -0,0 +1,60 @@
+/*
+
+Given an array nums of n integers where n > 1, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+
+Note: Please solve it without division and in O(n);
+
+*/
+
+// My solution, which has a quadratic time complexity;
+function productExceptSelf(arr) {
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    let product = 1;
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) continue;
+      let otherNum = arr[j];
+      product *= otherNum;
+    }
+    res.push(product);
+  }
+
+  return res;
+}
+
+// The top solution provided by LeetCode, which has a linear time complexity;
+// We are going to initialize an empty array, populate it with products given the circumstances, and then eventually return this populated array of integers;
+// We need to pass through the input array from left to right in order to generate the "forward products";
+// If the input array looks like this: [2, 7, 3, 4];
+// Then the forward products would be: [1, 2, 14, 42];
+// This is so because: [1*1, 1*2, 2*7, 2*7*3];
+// Now we need to grab the "backward products";
+// From iterating from right to left, we get: [84, 24, 56, 42];
+// This is so because: [12*7, 12*2, 4*14, 1*42];
+// When going forwards, we can ignore the value at the tail-end;
+// When going backwards, we can ignore the value at the head-end;
+// In any case, we are aiming to take the product of the numbers on either side of the partition point;
+function productExceptSelf(arr) {
+  let res = [];
+
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    res[i] = product;
+    product *= arr[i];
+  }
+
+  product = 1;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    res[i] *= product;
+    product *= arr[i];
+  }
+
+  return res;
+}
