9b1502d1bb564aa2b75f4e6c157af97f219257e2
Allan James Lapid <ajLapid718@gmail.com>
2018-08-29 22:46:26 -0400

    Implement an inorder traversal to validate a BST

---
+++ Top_Interview_Questions/validateBinarySearchTree.js
@@ -0,0 +1,56 @@
+/*
+
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+
+Input:
+    2
+   / \
+  1   3
+Output: true
+
+Example 2:
+    5
+   / \
+  1   4
+     / \
+    3   6
+Output: false
+
+Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+             is 5 but its right child's value is 4.
+
+*/
+
+function isValidBST(root) {
+  if (!root) return true;
+  return isSorted(inOrderTraversal(root));
+}
+
+function inOrderTraversal(node) {
+  let arr = [];
+  traverse(node, arr);
+  return arr;
+}
+
+function traverse(node, values) {
+  if (node.left) traverse(node.left, values);
+  values.push(node.val);
+  if (node.right) traverse(node.right, values);
+}
+
+function isSorted(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let curr = arr[i];
+    let folo = arr[i+1];
+    if (curr >= folo) return false;
+  }
+  return true;
+}
