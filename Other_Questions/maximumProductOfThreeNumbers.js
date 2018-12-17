b23736a366c3389111d5f6c8b5d176030075fd2d
Allan James Lapid <ajLapid718@gmail.com>
2018-12-17 01:07:47 -0500

    Establish control flow and conditional logic to get maximum product

--- Other_Questions/maximumProductOfThreeNumbers.js
+++ Other_Questions/maximumProductOfThreeNumbers.js
@@ -1,3 +1,22 @@
+/*
+
+Given an integer array, find three numbers whose product is maximum and output the maximum product.
+
+Example 1:
+Input: [1,2,3]
+Output: 6
+
+Example 2:
+Input: [1,2,3,4]
+Output: 24
+
+Note:
+
+- The length of the given array will be in range [3,104] and all elements are in the range [-1000, 1000].
+- Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
+
+*/
+
 // My Solution;
 // Time Complexity: O(3n), which simplifies to O(n);
 // Space Complexity: O(1);
@@ -28,14 +47,14 @@ function maximumProduct(nums) {
     let currNum = nums[i];
     if (i === primaryMaxIdx) continue;
 
-    if (currNum > secondaryMax && currNum <= primaryMax) {
-      secondaryMax = currNum;
-      secondaryMaxIdx = i;
-    }
-
     if (currNum < 0 && currNum < negPrimary) {
       negPrimary = currNum;
       negPrimaryIdx = i;
+    }
+
+    if (currNum > secondaryMax && currNum <= primaryMax) {
+      secondaryMax = currNum;
+      secondaryMaxIdx = i;
     }
   }
 
